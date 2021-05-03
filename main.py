89403b172f471800f2e19d940e0aeeee2178ddd8
thesilverpen <the.silver.pen@gmail.com>
2021-05-03 17:28:45 +0000

    added rules

--- main.py
+++ main.py
@@ -2,5 +2,18 @@
 #uses simple python code and concepts
 print("Welcome to Secret Word\n The rules are simple\n Don't be discouraged\n Play as many times as you like!")
 
+print("""
+---------------------------------------------------
+---------------------------------------------------
+Are you ready to find the secret word?
+Can you solve the clues?
+Let's begin!!!
+---------------------------------------------------
+---------------------------------------------------
+""")
+
+print("""Rules:\n 1 Read each clue and determine possible solutions 2 Do not cheat\n 3 Cheating is nearly impossible anyway!\n 4 Have fun even if you do not guess the secret word!""")
+
+
 
 
