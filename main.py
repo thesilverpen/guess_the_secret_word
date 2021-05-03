afcbbeca6e334c6b4e21a91fdada9aa502f2be20
thesilverpen <the.silver.pen@gmail.com>
2021-05-03 17:18:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+#a short, simple game to play
+#uses simple python code and concepts
+print("Welcome to Secret Word\n The rules are simple\n Don't be discouraged\n Play as many times as you like!")
+
+
+
